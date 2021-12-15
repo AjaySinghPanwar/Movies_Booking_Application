50f8079a8743fb97b123464be28942936e7f622a
Ajay Singh <a.panwar48656@gmail.com>
2021-12-15 14:30:50 +0530

    Movies Booking Frontend Added

---
+++ movieBookingFrontend/src/screens/Controller.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Home from '../screens/home/Home';
+import Details from '../screens/details/Details';
+import moviesData from '../common/movieData';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import BookShow from '../screens/bookshow/BookShow';
+import Confirmation from '../screens/confirmation/Confirmation';
+
+function Controller() {
+    return (
+      <Router>
+        <div className="main-container">
+          <Route exact path='/' render={(props) => <Home {...props} moviesData={moviesData} />} />
+          <Route path='/movie/:id' render={(props) => <Details {...props} />} />
+          <Route path='/bookshow/:id' render={(props) => <BookShow {...props} />} />
+          <Route path='/confirm/:id' render={(props) => <Confirmation {...props} />} />
+        </div>
+      </Router>
+    )
+}
+
+export default Controller;
